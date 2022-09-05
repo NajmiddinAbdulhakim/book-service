953d6ab369ae9e8643fc78bfebb3a848361eef1b
Najmiddin <najmiddin@example.com>
2022-09-05 15:08:11 +0500

    added connecting db

---
+++ db/connect.go
@@ -0,0 +1,46 @@
+package db
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/NajmiddinAbdulhakim/ude/book-service/config"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func ConnectDB(cfg config.Config) (*sqlx.DB, error) {
+	db, err := sqlx.Open("postgres",
+		fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+			cfg.PostgresHost, cfg.PostgresPort, cfg.PostgresUser, cfg.PostgresPassword, cfg.PostgresDB,
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	driver, err := postgres.WithInstance(db.DB, &postgres.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connecting migrate driver")
+	}
+	m, err := migrate.NewWithDatabaseInstance(
+		"file:/home/najmiddin/go/src/github.com/NajmiddinAbdulhakim/mybook/migrations",
+		"postgres", driver,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connecting migrate :%v", err)
+	}
+
+	if err = m.Up(); err != nil && errors.Is(err, migrate.ErrNoChange) {
+		return nil, fmt.Errorf("failed to migrate: %v", err)
+	}
+	return db, nil
+}
---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/NajmiddinAbdulhakim/ude/book-service
+
+go 1.18
+
+require (
+	github.com/NajmiddinAbdulhakim/mybook v0.0.0-20220809084943-dee61342ef3c
+	github.com/golang-migrate/migrate/v4 v4.15.2
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/lib/pq v1.10.6
+	github.com/spf13/cast v1.5.0
+)
+
+require (
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+)
